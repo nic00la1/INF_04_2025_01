bf246997c3d853bd9332978d34551cb8ce511db3
Nicola Kaleta <99048749+nic00la1@users.noreply.github.com>
2025-04-08 13:57:26 +0200

    Add files via upload

---
+++ mobilna/Song.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NF_04_2025_01_Music_Player
+{
+    public class Song
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Duration { get; set; }
+        public string CoverImage { get; set; }
+    }
+}
